16b2e764880dbbe6508cad0d62a381fbf7874459
조우경 <ghvmcm123@naver.com>
2022-01-26 22:48:02 +0900

    01.26

--- src/step09/소수.java
+++ src/step09/소수.java
@@ -2,28 +2,30 @@ package step09;
 
 import java.util.Scanner;
 
-public class 소수찾기 {
+public class 소수 {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int M = sc.nextInt();
 		int N = sc.nextInt();
 		
-		int count = 0;
-		for(int i=0;i<N;i++) {
-			int num = sc.nextInt();
+		int min = N;
+		int sum = 0;
+		
+		for(int i=M;i<=N;i++) {
 			int j=2;
-			while(j<num) {
-				if(num%j==0) {
+			while(j<i) {
+				if(i%j==0) {
 					break;
 				}
 				j++;
 			}
-			if(j==num) {
-				count++;
-			}
+			if()
+			
 		}
-		System.out.println(count);
+
 	}
+
 }
