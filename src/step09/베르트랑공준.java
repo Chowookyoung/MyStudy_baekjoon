5cd15bfcafc8d2af24123f7ce098014504d130d6
조우경 <ghvmcm123@naver.com>
2022-01-26 23:27:23 +0900

    기본수학2 - 소수구하기 시간초과 수정

---
+++ src/step09/베르트랑공준.java
@@ -0,0 +1,43 @@
+package step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 베르트랑공준 {
+	
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		
+		//베르트랑 공준은 임의의 자연수 n에 대하여, n보다 크고, 2n보다 작거나 같은 소수는 적어도 하나 존재한다는 내용
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = -1;
+		while(true) {
+			n = Integer.parseInt(bf.readLine());
+			if(n==0)
+				break;
+			int count = 0;
+			for(int i=n+1;i<=2*n;i++) {
+				int j=2;
+				while(j<i) {
+					if(i%j==0) {
+						break;
+					}
+					j++;
+				}
+				if(j==i) {
+					count++;
+				}
+				
+			}
+			sb.append(count+"\n");
+			
+		}
+		
+		System.out.println(sb);
+		
+	}
+}
--- src/step09/소수구하기.java
+++ src/step09/소수구하기.java
@@ -1,30 +1,39 @@
 package step09;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class 소수구하기 {
 
 	public static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] input = bf.readLine().split(" ");
 		
-	Scanner sc = new Scanner(System.in);
+		int M = Integer.parseInt(input[0]);
+		int N = Integer.parseInt(input[1]);
 		
-		int M = sc.nextInt();
-		int N = sc.nextInt();
-		
-		for(int i=M;i<=N;i++) {
-			int j=2;
-			while(j<i) {
-				if(i%j==0) {
+//		Scanner sc = new Scanner(System.in);
+//		int M = sc.nextInt();
+//		int N = sc.nextInt();
+
+		for (int i = M; i <= N; i++) {
+			int j = 2;
+			while (j < i) {
+				if (i % j == 0) {
 					break;
 				}
 				j++;
 			}
-			if(j==i) {
-				sb.append(i+"\n");
+			if (j == i) {
+				sb.append(i + "\n");
 			}
-			
+
 		}
 		System.out.println(sb);
 	}
