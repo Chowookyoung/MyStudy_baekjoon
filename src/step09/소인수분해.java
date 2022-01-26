782a2c4e13076343204c29b2e42fd221527216a4
조우경 <ghvmcm123@naver.com>
2022-01-26 23:06:23 +0900

    기본수학2 - 소인수분해

--- src/step09/소인수분해.java
+++ src/step09/소인수분해.java
@@ -1,11 +1,29 @@
 package step09;
 
+import java.util.Scanner;
+
 public class 소인수분해 {
 
+	public static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
-		
-		
 
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		if(N==1)
+			sb.append(1);
+		
+		int i = 2;
+		while (N > 1) {
+			if (N % i != 0) {
+				i++;
+			} else {
+				sb.append(i + "\n");
+				N /= i;
+			}
+		}
+		System.out.println(sb);
 	}
-
 }
