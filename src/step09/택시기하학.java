bbb16c4fb78b7db2cc71842f5132f208256936e9
조우경 <ghvmcm123@naver.com>
2022-01-27 18:44:03 +0900

    기본수학2 - 터렛

---
+++ src/step09/택시기하학.java
@@ -0,0 +1,13 @@
+package step09;
+
+public class 택시기하학 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/step09/터렛.java
@@ -0,0 +1,59 @@
+package step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 터렛 {
+
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<T;i++) {
+			
+			String[] input = br.readLine().split(" ");
+			
+			int x1 = Integer.parseInt(input[0]);
+			int x2 = Integer.parseInt(input[1]);
+			int r1 = Integer.parseInt(input[2]);
+			
+			int y1 = Integer.parseInt(input[3]);
+			int y2 = Integer.parseInt(input[4]);
+			int r2 = Integer.parseInt(input[5]);
+			
+			double distance = (double) Math.sqrt(Math.pow(Math.abs(x1-y1), 2) + Math.pow(Math.abs(x2-y2), 2));
+			int max = Math.max(r1, r2);
+			int min = Math.min(r1, r2);
+			
+			if(distance==0) {
+				if(r1!=r2) {
+					sb.append("0\n");
+				}else {
+					sb.append("-1\n");
+				}
+			}else if(distance>max) {
+				if(r1+r2<distance) {
+					sb.append("0\n");
+				}else if(r1+r2==distance) {
+					sb.append("1\n");
+				}else {
+					sb.append("2\n");
+				}
+			}else if(distance<max||distance==max) {
+				if(distance+min==max) {
+					sb.append("1\n");
+				}else if(distance+min<max) {
+					sb.append("0\n");
+				}else if(distance+min>max) {
+					sb.append("2\n");
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
