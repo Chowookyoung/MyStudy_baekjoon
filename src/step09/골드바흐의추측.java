1ca547b25017c4cc1fd220a9991e8a13caaea5c0
조우경 <ghvmcm123@naver.com>
2022-01-27 14:27:51 +0900

    01.27 백준 - 기본수학2

---
+++ src/step09/골드바흐의추측.java
@@ -0,0 +1,25 @@
+package step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 골드바흐의추측 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = br.read();
+		
+		for(int i = 0;i<T;i++) {
+			int n = br.read();
+			
+			
+			
+		}
+		
+		
+	}
+
+}
