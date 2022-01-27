4fc55e94b7406881b0c535ea1e89112273da84c5
조우경 <ghvmcm123@naver.com>
2022-01-27 14:42:47 +0900

    기본수학2 - 골드바흐의 추측

--- src/step09/골드바흐의추측.java
+++ src/step09/골드바흐의추측.java
@@ -1,24 +1,49 @@
 package step09;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class 골드바흐의추측 {
 
-	public static void main(String[] args) throws IOException{
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		
-		int T = br.read();
+		int T = sc.nextInt();
 		
 		for(int i = 0;i<T;i++) {
-			int n = br.read();
+			int n = sc.nextInt();
 			
-			
+			int n2 = n/2;
+			int j=0;
+			while(j<n/2) {
+				int k=2;
+				while(k<n2-j) {
+					if((n2-j)%k==0) {
+						break;
+					}
+					k++;
+				}
+				int l=2;
+				while(l<n2+j) {
+					if((n2+j)%l==0) {
+						break;
+					}
+					l++;
+				}
+				
+				if(k==n2-j&&l==n2+j) {
+					sb.append((n2-j)+" "+(n2+j)+"\n");
+					break;
+				}
+				
+				j++;
+			}
 			
 		}
 		
+		System.out.println(sb);
 		
 	}
 
