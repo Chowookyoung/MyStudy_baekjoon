22a401bfa3f279b188d022f2cb15f7d02252b7cf
조우경 <ghvmcm123@naver.com>
2022-01-27 18:55:46 +0900

    기본수학2 - 택시기하학

--- src/step09/택시기하학.java
+++ src/step09/택시기하학.java
@@ -1,13 +1,19 @@
 package step09;
 
+import java.util.Scanner;
+
 public class 택시기하학 {
 
 	public static void main(String[] args) {
 		
+		//택시 기하학에서 반지름은 D(T1,T2) = |x1-x2| + |y1-y2|
 		
-		
-		
+		Scanner sc = new Scanner(System.in);
 
+		int R = sc.nextInt();
+		
+		System.out.println(Math.pow(R, 2)*Math.PI);
+		System.out.println(Math.pow(R, 2)*2);
+		
 	}
-
 }
