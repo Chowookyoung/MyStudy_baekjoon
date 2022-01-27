0e249ee64d1a7fd52a0c28bf1ea1b623a0b327e6
조우경 <ghvmcm123@naver.com>
2022-01-27 14:51:24 +0900

    기본수학2 - 직사각형에서 탈출

--- src/step09/직사각형에서탈출.java
+++ src/step09/직사각형에서탈출.java
@@ -1,10 +1,22 @@
 package step09;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 직사각형에서탈출 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = (br.readLine()).split(" ");
+		
+		int x = Integer.parseInt(input[0]);
+		int y = Integer.parseInt(input[1]);
+		int w = Integer.parseInt(input[2]);
+		int h = Integer.parseInt(input[3]);
+		
+		System.out.println(Math.min(Math.min(x, w-x),Math.min(y,h-y)));
 	}
-
 }
